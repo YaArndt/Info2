455ce943127ee398dc447dd218555a5a242f50b8
Yannik Arndt <yannik.arndt@gmx.de>
2022-02-11 23:38:07 +0100

    Annotations

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from xwizard_templates import *
 from hamming import *
 
+# Requires Pyperclip:
+# pip install pyperclip
+
 if __name__ == "__main__":
     
     alph = ["a", "b"]
@@ -8,20 +11,22 @@ if __name__ == "__main__":
     
     states = num_states(7)
     
-    template = fsm_template(states, bin)
-    addToClipBoard(template)
-    
-    listed_hamming([
-        "1111000000000000",
-        "0000111100000000",
-        "0000000011110000",
-        "0000000000001111",
-        "1100110000000000",
-        "0000000011001100",
-        "0000110011000000",
-        "1100000000001100"
-    ])
+    # template = fsm_template(states, bin)
+    # addToClipBoard(template)
     
     
-    # pda_template_text = pda_template(states, alph, bin)
-    # addToClipBoard(pda_template_text)
+    
+    # listed_hamming([
+    #     "1111000000000000",
+    #     "0000111100000000",
+    #     "0000000011110000",
+    #     "0000000000001111",
+    #     "1100110000000000",
+    #     "0000000011001100",
+    #     "0000110011000000",
+    #     "1100000000001100"
+    # ])
+    
+    
+    pda_template_text = pda_template(states, alph, bin)
+    addToClipBoard(pda_template_text)
